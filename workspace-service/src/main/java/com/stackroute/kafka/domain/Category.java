dc320db9e540c70d33da7136d1509c3b50840a0d
Rutuja.Bacchuwar <rutuja.bacchuwar@stackroute.in>
2019-07-18 16:36:54 +0530

    added data

--- workspace-service/src/main/java/com/stackroute/kafka/domain/Category.java
+++ workspace-service/src/main/java/com/stackroute/kafka/domain/Category.java
@@ -9,18 +9,107 @@ import java.util.Arrays;
 @Data
 public class Category {
 
-    @Id
-    int categoryId;
-    String categoryName;
-    // BigDecimal price;
-    String[] additionalAmenities;
-    double hourlyPrice;
-    double monthlyPrice;
-    double dailyPrice;
-    int capacity;
-    String status="Available";
+
+        @Id
+        int categoryId;
+        String categoryName;
+        // BigDecimal price;
+        String[] additionalAmenities;
+        double hourlyPrice;
+        double monthlyPrice;
+        double dailyPrice;
+        int capacity;
+        String status="Available";
 
 
-    private Dimension dimension;   
+        private Dimension dimension;
 
-}
+        public Dimension getDimension() {
+            return dimension;
+        }
+
+        public void setDimension(Dimension dimension) {
+            this.dimension = dimension;
+        }
+
+        public Category() {
+        }
+
+        public Category(int categoryId, String categoryName, BigDecimal price, String[] additionalAmenities) {
+            this.categoryId = categoryId;
+            this.categoryName = categoryName;
+            // this.price = price;
+            this.additionalAmenities = additionalAmenities;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public void setCategoryId(int categoryId) {
+            this.categoryId = categoryId;
+        }
+
+        public void setCategoryName(String categoryName) {
+            this.categoryName = categoryName;
+        }
+
+        public void setAdditionalAmenities(String[] additionalAmenities) {
+            this.additionalAmenities = additionalAmenities;
+        }
+
+        public double getHourlyPrice() {
+            return hourlyPrice;
+        }
+
+        public void setHourlyPrice(double hourlyPrice) {
+            this.hourlyPrice = hourlyPrice;
+        }
+
+        public double getMonthlyPrice() {
+            return monthlyPrice;
+        }
+
+        public void setMonthlyPrice(double monthlyPrice) {
+            this.monthlyPrice = monthlyPrice;
+        }
+
+        public double getDailyPrice() {
+            return dailyPrice;
+        }
+
+        public void setDailyPrice(double dailyPrice) {
+            this.dailyPrice = dailyPrice;
+        }
+
+        public int getCapacity() {
+            return capacity;
+        }
+
+        public void setCapacity(int capacity) {
+            this.capacity = capacity;
+        }
+
+        public int getCategoryId() {
+            return categoryId;
+        }
+
+        public String getCategoryName() {
+            return categoryName;
+        }
+
+//    public BigDecimal getPrice() {
+//        return price;
+//    }
+
+        public String[] getAdditionalAmenities() {
+            return additionalAmenities;
+        }
+
+
+    }
+
